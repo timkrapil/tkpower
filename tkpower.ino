d647a7384eacab37ff091d466bcdc75b49a60b83
timkrapil <tim@Tims-MacBook-Pro.local>
2015-09-11 20:53:17 -0400

    added pin labels

--- tkpower.ino
+++ tkpower.ino
@@ -2,29 +2,29 @@
 
 #include "EmonLib.h"                   // Include Emon Library
 EnergyMonitor emon1;                   // Create an instance
-EnergyMonitor emon0; 
+EnergyMonitor emon0;
 
 void setup()
-{  
+{
   Serial.begin(9600);
-  
+
   emon1.current(1, 100);             // Current: input pin, calibration.
-  emon0.current(0, 100); 
+  emon0.current(0, 100);
 }
 
 void loop()
 {
   double Irms1 = emon1.calcIrms(7680);  // Calculate Irms only
-  
-  Serial.println("pin1");
+  Serial.print ("pin1 ");
   Serial.print(Irms1*122.0);         // Apparent power
   Serial.print(" ");
   Serial.println(Irms1);           // Irms
   double Irms0 = emon0.calcIrms(7680);
-  Serial.println("pin0");
+  Serial.print("pin0 ");
   Serial.print(Irms0*122.0);         // Apparent power
   Serial.print(" ");
   Serial.println(Irms0);           // Irms
 
-  
+
+
 }
