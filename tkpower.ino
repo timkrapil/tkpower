b684a5862e1e4ecc7c75b756f7d31d4db3a6431b
timkrapil <tim@Tims-MacBook-Pro.local>
2015-10-04 23:57:30 -0400

    updated calibration and measurements per reading

--- tkpower.ino
+++ tkpower.ino
@@ -8,23 +8,28 @@ void setup()
 {
   Serial.begin(9600);
 
-  emon1.current(1, 100);             // Current: input pin, calibration.
-  emon0.current(0, 100);
+  emon1.current(1, 90.91);             // Current: input pin, calibration.
+  emon0.current(0, 90.91);
 }
 
 void loop()
 {
-  double Irms1 = emon1.calcIrms(7680);  // Calculate Irms only
+  double Irms1 = emon1.calcIrms(5920);  // Calculate Irms only
+  double Irms0 = emon0.calcIrms(5920);
   Serial.print ("pin1 ");
   Serial.print(Irms1*122.0);         // Apparent power
   Serial.print(" ");
-  Serial.println(Irms1);           // Irms
-  double Irms0 = emon0.calcIrms(7680);
+  Serial.print(Irms1);
+  Serial.print(" ");
   Serial.print("pin0 ");
   Serial.print(Irms0*122.0);         // Apparent power
   Serial.print(" ");
-  Serial.println(Irms0);           // Irms
-
-
+  Serial.print(Irms0);
+  Serial.print(" ");
+  Serial.print((Irms0*122.0)+(Irms1*122.0));
+  Serial.print(" ");
+  Serial.println(Irms0+Irms1);
+  
+ 
 
 }
