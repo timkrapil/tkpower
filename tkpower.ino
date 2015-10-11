ef4938b8c8473b25fcc84ac3a9be1ab7ad66fbfd
timkrapil <tim@Tims-MacBook-Pro.local>
2015-10-10 21:10:08 -0400

    changes for regulated model

--- tkpower.ino
+++ tkpower.ino
@@ -8,14 +8,14 @@ void setup()
 {
   Serial.begin(9600);
 
-  emon1.current(1, 90.91);             // Current: input pin, calibration.
-  emon0.current(0, 90.91);
+  emon1.current(1, 19.5);             // Current: input pin, calibration.
+  emon0.current(0, 19.5);
 }
 
 void loop()
 {
-  double Irms1 = emon1.calcIrms(5920);  // Calculate Irms only
-  double Irms0 = emon0.calcIrms(5920);
+  double Irms1 = emon1.calcIrms(3410);  // Calculate Irms only
+  double Irms0 = emon0.calcIrms(3410);
   Serial.print ("pin1 ");
   Serial.print(Irms1*122.0);         // Apparent power
   Serial.print(" ");
