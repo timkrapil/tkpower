e3c90dc230c37eac3117de7e47f4638e69238e8f
timkrapil <tim@Tims-MacBook-Pro.local>
2015-10-10 23:10:33 -0400

    added service setup

--- tkpower_install.sh
+++ tkpower_install.sh
@@ -7,3 +7,21 @@ sudo apt-get install -y build-essential python-dev python-rpi.gpio nodejs netata
 
 
 sudo npm install forever -g
+
+npm install
+
+sudo cp /home/pi/tkpower/tkpower-plotwatt
+sudo chmod a+x /etc/init.d/tkpower-plotwatt
+sudo update-rc.d tkpower-plotwatt defaults
+
+sudo cp /home/pi/tkpower/tkpower-loggly
+sudo chmod a+x /etc/init.d/tkpower-loggly
+sudo update-rc.d tkpower-loggly defaults
+
+sudo cp /home/pi/tkpower/tkpower-logger
+sudo chmod a+x /etc/init.d/tkpower-logger
+sudo update-rc.d tkpower-logger defaults
+
+sudo cp /home/pi/tkpower/tkpower-collector
+sudo chmod a+x /etc/init.d/tkpower-collector
+sudo update-rc.d tkpower-collector defaults
